8655b4865270bc7f1826728b2e877ed6c678cdab
Reyaz-Ansari <mdreyaz7079@gmail.com>
2024-11-02 17:23:46 +0530

    first commit next

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import pic4 from "../../Assets/headphone4.png";
 
 function Banner() {
   return (
-    <div className='h-[500px] bg-gray-200'>Banner</div>
+    <>
+    <div className='h-[500px] bg-gray-200'>Banner
+    
+    </div>
+    <img src={pic4} alt='' />
+    </>
+    
   )
 }
 
