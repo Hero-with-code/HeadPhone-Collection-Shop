8cdd5a51b78498d705002528174b1abc538c7be6
Reyaz-Ansari <mdreyaz7079@gmail.com>
2024-11-02 13:18:01 +0530

    first commit next

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import fanta_logo from "../../Assets/fanta_logo.png";
+import logo from "../../Assets/logo.png";
 
 function Navbar() {
   return (
     <>
-       <div className="fixed w-full top-0 left-0 z-20 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
+       <div className="fixed w-full top-0 left-0 z-20 shadow-md bg-white">
         <div className='container py-3 sm:py-0'>
-            <div className='flex justify-between items-center'>
+            <div className='flex items-center justify-between'>
+                {/* LOGO SECTION */}
                 <div>
-                    <Link to="#" className="flex items-center gap-2 text-2xl sm:text-3xl font-bold ps-10">
-                    <img src={fanta_logo} alt='logo' className='w-[80px] h-[60px] text-center' /> Fanta Shop
+                    <Link to="#" className="flex items-center gap-4 text-2xl sm:text-3xl font-bold ps-10">
+                    <img src={logo} alt='logo' className='w-[80px] h-[60px] text-center' /> Collection
                     </Link>
                 </div>
+                {/* MENU SECTION */}
                 <div className="flex items-center gap-4">
                     
                     <ul className='hidden sm:flex gap-4'>
@@ -21,10 +23,7 @@ function Navbar() {
                         </li>
                         <li>
                             <Link to="/About" className="inline-block py-4 px-4 hover:text-primary">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/Contact" className="inline-block py-4 px-4 hover:text-primary">Contact</Link>
-                        </li>
+                        </li>                   
                         <li>
                             <Link to="/Categories" className="inline-block py-4 px-4 hover:text-primary">Categories</Link>
                         </li>
@@ -32,9 +31,13 @@ function Navbar() {
                             <Link to="/Blog" className="inline-block py-4 px-4 hover:text-primary">Blog</Link>
                         </li>
                         <li>
-                            <Link to="/Login" className="inline-block py-4 px-4 hover:text-primary"><button>Admin</button></Link>
+                            <Link to="/Contact" className="inline-block py-4 px-4 hover:text-primary">Contact</Link>
                         </li>
                     </ul>
+                </div>
+                {/* ICON HEADPHONE SECTION */}
+                <div>
+                <Link to="/Login" className="inline-block py-4 px-4 hover:text-primary"><button>User</button></Link>
                 </div>
             </div>
         </div>
